4c23a9afea0e0c94bfcd64ab14d7785f2abccc39
Yuta Tokitake <toki211@gmail.com>
2014-08-21 14:13:23 +0900

    add some missing files

--- app/src/main/java/jp/icecreamparfait/intern/cyberagent/holidayin/DetailActivity.java
+++ app/src/main/java/jp/icecreamparfait/intern/cyberagent/holidayin/DetailActivity.java
@@ -4,14 +4,10 @@ import java.util.Locale;
 
 import android.app.TabActivity;
 import android.os.Bundle;
-import android.app.ActionBar;
 import android.app.Fragment;
 import android.app.FragmentManager;
-import android.app.FragmentTransaction;
 import android.support.v13.app.FragmentPagerAdapter;
-import android.os.Bundle;
 import android.support.v4.view.ViewPager;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -19,7 +15,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TabHost;
 import android.widget.TabHost.TabSpec;
-import android.widget.TextView;
 
 
 public class DetailActivity extends TabActivity {
@@ -170,7 +165,7 @@ public class DetailActivity extends TabActivity {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_my, container, false);
+            View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
             return rootView;
         }
     }
